9d47483e37b3d63d6504c4a1f90126207338f118
OZVE <osman.13914@gmail.com>
2021-08-30 19:32:09 -0300

    not work

--- Clase11/public/index.js
+++ Clase11/public/index.js
@@ -1,6 +1,32 @@
-const socket = io();
+document.addEventListener('DOMContentLoaded', function() {
+    
+    const socket = io()
+    const guardarProd = document.getElementById("Guardarform")
+    const title = document.getElementById("title")
+    const price = document.getElementById("price")
+    const thumb = document.getElementById("thumbnail")
+    // const template = document.getElementById("template")
+    // const toRender = document.getElementById("toRender")
+    socket.on("message", msg => {
+        console.log(msg)
+    })
 
-socket.on('message',(data)=>{
-    console.log(data);
-    socket.emit('hello','hola...')
-})
+
+
+function enviarProducto (){
+   
+    var url = 'http://localhost:8000/api/productos/guardarform';
+    var data = {title, price,thumb};
+    
+    fetch(url, {
+      method: 'POST', 
+      body: JSON.stringify(data), 
+      headers:{
+        'Content-Type': 'application/json'
+      }
+    }).then(res => res.json())
+    .catch(error => console.error('Error:', error))
+    .then(response => console.log('Success:', response));
+
+ };
+});
