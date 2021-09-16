748e09d8b3934a94df55dc79e52c9a4f3fe19f41
Osman Ramirez <osman.13914@gmail.com>
2021-09-15 22:25:53 -0300

    condicionales agregados

--- Clase17/controllers/serverdb.js
+++ Clase17/controllers/serverdb.js
@@ -19,25 +19,56 @@ function guardarProducto (name, description, code, picture, price, stock) {
   
 }
 
+knex.schema.hasTable('lista').then(function(exist){
+  if(!exist){
+   return knex.schema.createTable('lista', (table)=> {
+          table.increments("id").primary();
+          table.datetime("date", { precision: 6 }).defaultTo(knex.fn.now(6));
+          table.integer("qty").notNullable();
+          table.string("name", 100).notNullable();
+          table.string("description", 250);
+          table.integer("code").notNullable();
+          table.string("picture", 255);
+          table.integer("price").notNullable();
+          table.integer("stock").notNullable();
+      }).then(console.log('tabla creada'))
+    }
+});
 
 
-// knex.schema.createTable('lista', (table)=> {
-//     table.increments("id").primary();
-//     table.datetime("date", { precision: 6 }).defaultTo(knex.fn.now(6));
-//     table.integer("qty").notNullable();
-//     table.string("name", 100).notNullable();
-//     table.string("description", 250);
-//     table.integer("code").notNullable();
-//     table.string("picture", 255);
-//     table.integer("price").notNullable();
-//     table.integer("stock").notNullable();
-// }).then (
-//     (console.log('tabla creada'),
 
 
-//     (err) => console.log(err),
-//     () => knex.destroy())
-// )
 
 
+
+knex.schema.hasTable('carrito').then(function(exist){
+  if(!exist){
+   return knex.schema.createTable('carrito', (table)=> {
+        table.increments("id").primary();
+        table.datetime("date", { precision: 6 }).defaultTo(knex.fn.now(6));
+    }).then (
+        (console.log('tabla creada'),
+    
+        (err) => console.log(err),
+        () => knex.destroy())
+      )
+  }
+})
+
+knex.schema.hasTable('union').then(function(exist){
+  if(!exist){
+   return knex.schema.createTable('relprod', (table)=> {
+          table.integer("id-carrito");
+          table.integer("id-producto");
+          table.integer("qty");
+          table.datetime("date", { precision: 6 }).defaultTo(knex.fn.now(6));
+      }).then (
+          (console.log('tabla creada'),
+      
+    
+        (err) => console.log(err),
+        () => knex.destroy())
+      )
+  }
+})  
 module.exports = {guardarProducto, knex, conectarMaria}
