f4391748fc379b48768a940a0c25c2eb0d250ecc
unknown <osman.13914@gmail.com>
2022-01-08 12:44:37 -0300

    clase46

--- Clase46/app.js
+++ Clase46/app.js
@@ -1,253 +1,54 @@
-//DEPENDENCIES
-import express from 'express';
-import http from 'http';
-import cookieParser from 'cookie-parser';
-import session from 'express-session';
-import MongoStore from 'connect-mongo';
-import moment from 'moment';
-import handlebars from 'express-handlebars';
-import { Server as Socket } from 'socket.io';
-import passport from 'passport';
-import { Strategy as FacebookStrategy } from 'passport-facebook';
-import cluster from 'cluster';
-import compression from 'compression';
-import  Log4js  from 'log4js';
-import  fork  from 'child_process';
-import * as os from 'os';
-
-//----------------------------------------//
-const CON_CHILD_PROCESS_FORK = false;
-//CLASSES AND DB
-import Products from './api/products.js';
-import Messages from './api/messages.js';
-import { MongoDB} from './DB/db.js';
-//----------------------------------------------//
-
-//SERVER SETTINGS
-const app = express()
-const server = http.Server(app);
-const io = new Socket(server)
-const router = express.Router();
-app.use('/api', router);
-app.use(express.static('public'));
-router.use(express.json());
-router.use(express.urlencoded({extended: true}));
-const PORT = process.env.PORT || 8080;
-const srv = server.listen(PORT, async () =>{
-  console.log(`Sever listen port number ${srv.address().PORT}`);
-  try{
-    const mongo = new MongoDB('mongodb://localhost/ecommerce');
-    await mongo.connect();
-    console.log('Mongo DB connected');
-  }catch(err){
-    console.log(`Mongo DB Error connection : ${err}`);
-
-  }
-});
-srv.on('error', err => console.log(`Server error ${err}`));
-//------------------------------------//
-
-//COMPRESSION
-app.use(compression());
-//--------------------//
-
-//LOGGERS
-Log4js.configure({
-    appenders: {
-      miLoggerConsole: { type: "console"},
-      miLoggerFileWarn: { type: 'file', filename: 'warn.log'},
-      miLoggerFileError: {type: 'file', filename: 'error.log'}
-    },
-    categories:{
-      default: {appenders: ['miLoggerConsole'], level: 'trace'},
-      info: {appenders: ["miLoggerConsole"], level: "info"},
-      warn: {appenders: ["miLoggerFileWarn"], level: "warn"},
-      error: { appenders: ["miLoggerFileError"], level: "error"}
-    }
-});
-const loggerInfo = Log4js.getLogger('info');
-const loggerWarn = Log4js.getLogger('warn');
-const loggerError = Log4js.getLogger('Error');
-//PASSPORT
-const numCPUs = os.cpus().length;
-const ClusterMode = process.argv[5] == 'CLUSTER';
-//MASTER
-if(ClusterMode && cluster.isMaster){
-
-  loggerInfo.info(`CPUs Number: ${numCPUs}`);
-  loggerInfo.info(`PID MASTER ${process.pid}`);
-
-  for(let i = 0; i<numCPUs; i++){
-    cluster.fork();
-  }
-
-  cluster.on('exit', worker =>{
-    loggerInfo.info('Worker', worker.process.pid, 'died', new Date().toLocaleString())
-    cluster.fork()
-  })
-}else{
+/**
+ * app.js
+ *
+ * Use `app.js` to run your app without `sails lift`.
+ * To start the server, run: `node app.js`.
+ *
+ * This is handy in situations where the sails CLI is not relevant or useful,
+ * such as when you deploy to a server, or a PaaS like Heroku.
+ *
+ * For example:
+ *   => `node app.js`
+ *   => `npm start`
+ *   => `forever start app.js`
+ *   => `node debug app.js`
+ *
+ * The same command-line arguments and env vars are supported, e.g.:
+ * `NODE_ENV=production node app.js --port=80 --verbose`
+ *
+ * For more information see:
+ *   https://sailsjs.com/anatomy/app.js
+ */
 
 
-const FACEBOOK_CLIENT_ID = '644500950258395';
-const FACEBOOK_CLIENT_SECRET = 'dfe2216da6d5e10941619f316d7398fd';
-passport.use(new FacebookStrategy({
-  clientID: FACEBOOK_CLIENT_ID,
-  clientSecret: FACEBOOK_CLIENT_SECRET,
-  callbackURL:'/auth/facebook/callback',
-  profileFields: ['id', 'displayName', 'photos','emails'],
-  scope: ['email']
-}, function(accessToken,refreshToken, profile, done){
-  let userProfile = profile;
-  return done(null, userProfile);
-}));
-
-passport.serializeUser(function(user, cb){
-  cb(null,user);
-})
-passport.deserializeUser(function(obj, cb) {
-  cb(null, obj);
-});
-//---
-//DECLARE NEW CLASS
-let products = new Products();
-let messages = new Messages();
-//--------------------------//
-
-// DATE CONVERTER
-var date = new Date();
-var dateConverted = moment(date).format('lll');
-//------------------------------------------//
-
-//COOKIE PARS, SESSION SETTINGS AND GET REQ
-app.use(cookieParser());
-app.use(session({
-  store: MongoStore.create({
-    mongoUrl: 'mongodb+srv://mongodbatlasoz:ozmongodbatlas@cluster0.ggzf8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    ttl: 600
-  }),
-  secret:'123',
-  resave: true,
-  saveUninitialized: false,
-  rolling: true,
-  cookie: {
-    maxAge: 60000
-  }
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-//----------------------------------------------------------------//
-
-//HBS CONFIGURATION
-app.engine(
-  'hbs',
-  handlebars({
-    extname: '.hbs',
-    defaultLayout: 'index.hbs'
-  })
-)
-app.set('view engine', 'hbs');
-app.set('views', './views');
-app.use(express.static('public'))
-//---------------------------//
-
-//USER LOGIN AND LOGOUT
-
-app.get('/login',(req,res) =>{
-  if(req.isAuthenticated()){
-    res.render('home', {
-      name: req.user.displayName,
-      photo: req.user.photos[0].value,
-      email: req.user.emails[0].value,
-      contador: req.user.contador,
-    });
-  }else{
-    res.sendFile(process.cwd() + '/public/login.html')
-  }
-})
-
-app.get('/auth/facebook', passport.authenticate('facebook'));
-app.get('/auth/facebook/callback', passport.authenticate('facebook',
-{successRedirect: '/home',
-failureRedirect: '/faillogin'
-}));
-
-app.get('/home', (req,res)=>{
-  res.redirect('/');
+// Ensure we're in the project directory, so cwd-relative paths work as expected
+// no matter where we actually lift from.
+// > Note: This is not required in order to lift, but it is a convenient default.
+process.chdir(__dirname);
 
 
-});app.get('/chat', (req,res)=>{
-  res.redirect('/home');
-});
 
-app.get('/faillogin', (req,res)=>{
-  res.render('login-error',{});
-});
+// Attempt to import `sails` dependency, as well as `rc` (for loading `.sailsrc` files).
+var sails;
+var rc;
+try {
+  sails = require('sails');
+  rc = require('sails/accessible/rc');
+} catch (err) {
+  console.error('Encountered an error when attempting to require(\'sails\'):');
+  console.error(err.stack);
+  console.error('--');
+  console.error('To run an app using `node app.js`, you need to have Sails installed');
+  console.error('locally (`./node_modules/sails`).  To do that, just make sure you\'re');
+  console.error('in the same directory as your app and run `npm install`.');
+  console.error();
+  console.error('If Sails is installed globally (i.e. `npm install -g sails`) you can');
+  console.error('also run this app with `sails lift`.  Running with `sails lift` will');
+  console.error('not run this file (`app.js`), but it will do exactly the same thing.');
+  console.error('(It even uses your app directory\'s local Sails install, if possible.)');
+  return;
+}//-•
 
-app.get('/logout', (req,res) =>{
-  let name = req.user.displayName
-  req.logOut();
-  res.render('logout',{name});
-});
-//-------------------------------------------//
 
-//ROUTES
-router.get('/products/list', async (req, res) =>{
-  res.json(await products.listAll())
-});
-
-router.get('/products/list/:id', async(req,res) =>{
-  let {id} = req.params
-  res.json(await products.listar(id))
-});
-
-router.post('/products/save', async (req,res) =>{
-  let product = req.body
-  await products.save(product)
-  res.json(product)
-});
-
-router.put('/products/update/:id', async (req,res) => {
-  let {id} = req.params
-  let product = req.body
-  await products.update(product, id)
-  res.json(product)
-});
-
-router.delete('/products/delete/:id', async (req,res) => {
-  let {id} = req.params
-  let product = await products.delete(id)
-  res.json(product)
-});
-
-router.get('/products/view', async (req,res) =>{
-  let prods = await products.listAll()
-  res.render('view',{
-    products: prods,
-    theresProducts: prods.length
-  })
-});
-//--------------------------------------------------------------//
-//WEB SOCKET SOCKET.IO
-io.on("connection", async socket => {
-  console.log("Somebody connected");
-    //send messages to the new client
-  socket.emit("products", await products.get());
-
-  //listen messages and send to everybody
-  socket.on("update", async data => {
-     if(data = 'ok'){
-       io.sockets.emit("products", await products.get());
-       
-     }
-});
-//---------------------------------------------------//
-
-//MESSAGES SETTING SOCKET.IO
-socket.emit('messages', await messages.getAll());
-
-socket.on('new-message', async data => {
-  await messages.save(data);
-  io.socket.emit('messages', await messages.getAll());
-})
-});
-}
+// Start server
+sails.lift(rc('sails'));
